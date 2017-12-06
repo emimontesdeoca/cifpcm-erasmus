e3b3455475e2ef84bdb7d6007be9780440fd903c
Emiliano Montesdeoca <emontesdeoc@gmail.com>
2017-12-06 20:37:05 +0000

    JSDoc added

--- js/movilidad.js
+++ js/movilidad.js
@@ -1,15 +1,43 @@
-function movilidad(tipo, ciclo, pais, ciudad) {
-  /// ID necesario para realizar cambios
-  this.id =
-    Math.random()
-      .toString(36)
-      .substring(7) || "a3k9ka";
-  /// Tipo
-  this.tipo = tipo || "tipo";
-  /// ciclo
-  this.ciclo = ciclo || "ciclo_" + this.id;
-  /// pais
-  this.pais = pais || "Uruguay";
-  /// ciudad
-  this.ciudad = ciudad || "Punta del Este";
+/** Clase movilidad. */
+class movilidad {
+  /**
+   * Create una movilidad
+   * @param {string} tipo Tipo de movilidad
+   * @param {string} ciclo ciclo de movilidad
+   * @param {string} pais Pais de la movilidad
+   * @param {string} ciudad Ciudad de la movilidad
+   */
+  constructor(tipo, ciclo, pais, ciudad) {
+    /// ID necesario para realizar cambios
+    this.id =
+      Math.random()
+        .toString(36)
+        .substring(7) || "a3k9ka";
+    /// Tipo
+    this.tipo = tipo || "tipo";
+    /// ciclo
+    this.ciclo = ciclo || "ciclo_" + this.id;
+    /// pais
+    this.pais = pais || "Uruguay";
+    /// ciudad
+    this.ciudad = ciudad || "Punta del Este";
+  }
+  /**
+   * Modificar el objeto movilidad
+   * @param {string} tipo Tipo de movilidad
+   * @param {string} ciclo ciclo de movilidad
+   * @param {string} pais Pais de la movilidad
+   * @param {string} ciudad Ciudad de la movilidad
+   *
+   */
+  modificar(tipo, ciclo, pais, ciudad) {
+    /// Tipo
+    this.tipo = tipo;
+    /// ciclo
+    this.ciclo = ciclo;
+    /// pais
+    this.pais = pais;
+    /// ciudad
+    this.ciudad = ciudad;
+  }
 }
